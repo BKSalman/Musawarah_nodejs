9ce962da05182ed487a3d4dc743607dbf4c9eb01
Salman Abuhaimed <85521119+BKSalman@users.noreply.github.com>
2021-12-22 12:16:13 +0300

    .

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ require('dotenv').config({path:'.env'});
 
 const express = require('express'),
     mongoose = require('mongoose');
+
+const app = express(),
+    port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('wow')
+})
+
+app.listen(port, () => {
+    console.log('run server')
+})
